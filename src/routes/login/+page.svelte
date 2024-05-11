24b554b29f8264943387fdcba122c57368582ffa
SzP <szpimportant@gmail.com>
2024-05-11 21:13:14 +0200

    fixed a bug that caused vercel deploy to fail

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { supabase } from "$lib/supaBaseClient.js"
+    import { supabase } from "$lib/supabaseClient.js"
 	import { goto } from "$app/navigation"
     
     let email = ""
