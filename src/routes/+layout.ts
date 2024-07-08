6d93936a236795d977c415e568cd90defb0bb134
SzP <szpimportant@gmail.com>
2024-07-08 07:48:38 +0200

    Maybe fixes the bug

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,10 @@
 import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
+// import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 import type { LayoutLoad } from './$types'
+
+const PUBLIC_SUPABASE_URL='https://osqqfemzutaowfkjlymx.supabase.co'
+const PUBLIC_SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9zcXFmZW16dXRhb3dma2pseW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzMzkyMTcsImV4cCI6MjAzMDkxNTIxN30.AkhcJ04st3OQ7QYz7224YIcdZeynLYO6aKFPw_Jsasg'
+
 
 export const load: LayoutLoad = async ({ data, depends, fetch }) => {
   /**
