852dec4529de9628a75e1ac6ca81cdf48ffe9f0a
AlmaDav <alma.fuzi.david@gmail.com>
2024-08-30 20:38:15 +0200

    Finalized contact and Footer is reusable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
         if (data['session']) {goto('/app')}
     })
     import Navbar from './navbar.svelte';
+    import Footer from './footer.svelte';
 </script>
 
 <Navbar />
@@ -202,28 +203,4 @@
     
 </section>
 
-<footer class=" pt-5 lg:pt-10 bg-dark_bg text-bright_text" id="footer"> 
-    <div class="container ">
-        <div class="row">
-            <div class="col w-6/12 ml-5">
-                <h1 class="text-muted text-3xl"> Reach StyleSwipe:</h1> <!--TODO Change to logo?-->
-            </div>
-            <div class="col w-6/12 ml-16">  
-                <div class="gap-4">
-                    <div><a href="https://facebook.com"target="_blank">
-                        <i class="text-2xl underline">Facebook</i>
-                    </a></div>
-                    <div><a href="https://twitter.com" target="_blank">
-                        <i class="text-2xl fab fa-twitter underline">Twitter</i>
-                    </a></div>
-                    <div><a href="https://instagram.com" target="_blank">
-                        <i class="text-2xl fab fa-instagram fa-3x underline">Instagram</i>
-                    </a></div>
-                    <div><a href="https://linkedin.com" target="_blank">
-                        <i class="text-2xl fab fa-linkedin fa-3x underline">LinkedIn</i>
-                    </a></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
+<Footer />
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -7,6 +7,7 @@
 
     let formResponse = null;
     import Navbar from '../navbar.svelte';
+    import Footer from '../footer.svelte';
 
 </script>
 
@@ -36,7 +37,7 @@
     <div class="flex justify-center">
         <div class="grid grid-cols-2 align-middle mx-10 text-center gap-x-7 text-dark_text md:gap-x-12 lg:gap-x-20 gap-y-6 xl:gap-x-30 xl:mx-20"> <!--Main grid-->
             <div class="flex justify-center">
-                <div class="grid grid-cols-1 gap-y-6 w-1/2 "> <!--Left info-->
+                <div class="grid grid-cols-1 mx-auto gap-y-6 lg:w-1/2"> <!--Left info-->
                     <p class="mb-4 text-muted border-2 border-black rounded-xl mt-6 bg-dark_bg text-bright_text p-2">We offer numerous models to display your clothes on our platform, each flexible and completely free</p>
                     <p class="mb-4 text-muted border-2 border-black rounded-xl  bg-dark_bg text-bright_text p-2">StyleSwipe transfers our traffic to your website, converting it to active shoppers</p>
                     <p class="mb-4 text-muted border-2 border-black rounded-xl  bg-dark_bg text-bright_text p-2">We deliver you enormous order quantities, as a capsule wardrobe typically contains 8 to 15 pieces of clothes offered by your brand</p>
@@ -44,30 +45,39 @@
                     <p class="mb-4 text-muted border-2 border-black rounded-xl  bg-dark_bg text-bright_text p-2">If you want to build a more environmentally conscious image of your brand, and sell a lot of clothes at the same time, StyleSwipe is the perfect match for you</p>
                 </div>
             </div>
-            <div class="grid grid-cols-1"> <!--Right form-->
-                <h1 class="mt-8">Discuss the opportunities!</h1>
-                <form method="post" use:enhance class="grid grid-col-1 gap-y-6">
-                <div>   <label for="firstname">First name</label>
-                    <input type="text" name="firstname" id="firstname" required placeholder="John">
+            <div class="grid grid-cols-1 text-bright_text"> <!--Right form-->
+                <div class="bg-dark_bg rounded-xl mx-auto border-2 border-black p-8">
+                    <h1 class="text-center mb-8">Discuss the opportunities!</h1>
+                    <form method="post" use:enhance class="grid grid-col-1 gap-y-6">
+                        <div>
+                            <label for="firstname">First name</label>
+                            <input class="text-dark_text" type="text" name="firstname" id="firstname" required placeholder="John">
+                        </div>
+                        <div>
+                            <label for="lastname">Last name</label>
+                            <input class="text-dark_text" type="text" name="lastname" id="lastname" required placeholder="Smith">
+                        </div>
+                        <div>
+                            <label for="jobtitle">Job title</label>
+                            <input class="text-dark_text" type="text" name="jobtitle" id="jobtitle">
+                        </div>
+                        <div>
+                            <label for="email">E-mail:</label>
+                            <input class="text-dark_text" type="email" name="email" id="email" placeholder="xyz@company.com" required autocomplete="email">
+                        </div>
+                        <div>
+                            <label for="company">Company name</label>
+                            <input class="text-dark_text" type="text" name="company" id="company">
+                        </div>
+                        <div>
+                            <label for="message">Message</label>
+                            <textarea class="text-dark_text" name="message" id="message"></textarea>
+                        </div>
+                        <button type="submit" onclick="togglesubmit()" class="justify-center flex">
+                            <p class="line-clamp-2 mb-4 text-muted border-2 border-black rounded-xl bg-bright_bg text-dark_text hover:bg-dark_bg hover:text-bright_text w-1/2 p-2" id="submitbut">Submit</p>
+                        </button>
+                    </form>
                 </div>
-                <div>    <label for="lastname">Last name</label>
-                    <input type="text" name="lastname" id="lastname" required placeholder="Smith">
-                </div>
-                <div>    <label for="jobtitle">job title</label>
-                    <input type="text" name="jobtitle" id="job title">
-                </div>
-                <div>    <label for="email">E-mail: </label>
-                    <input type="email" name="email" id="email" placeholder="xyz@company.com" required autocomplete="email">
-                </div>
-                <div>    <label for="company">Company name</label>
-                    <input type="text" name="company" id="company">
-                </div>
-                <div>    <label for="message">message</label>
-                    <textarea name="message" id="message"></textarea>
-                </div>
-                <button onclick="togglesubmit()" class="justify-center flex"><p class="line-clamp-2 mb-4 text-muted border-2 border-black rounded-xl bg-bright_bg text-dark_text hover:bg-dark_bg hover:text-bright_text w-1/2 p-2" id="submitbut">Submit</button>
-                
-                </form>
             </div>
         </div>
     </div>
@@ -88,6 +98,32 @@
             </div>
         </div>
     </div>
-
+    <section class=" bg-bright_bg text-dark_text text-xl justify-center flex text-center py-16 lg:py-32"> <!--Certifications-->
+        <div class="container">
+            <div class="row">
+                <div class="text-dark_text">
+                    <div class="text-2xl lg:text-3xl">Certifications</div>
+                    <div class="container pb-8 pt-6">
+                        <div class="flex">
+                            <div class="w-full max-w-screen-lg mx-auto px-4">
+                                <div class="bg-dark_bg text-bright_text rounded-3xl px-4 lg:px-8 py-1 w-8/12 md:w-6/12 mx-auto border-4 border-black">
+                                    <h1 class="pt-2 lg:pt-4 mb-8 text-bright_text text-md lg:text-2xl text-center">
+                                        2nd place on Startup Challenge by
+                                    </h1>
+                                    <div class="flex justify-center space-x-20 xl:space-x-24 pb-2 lg:pb-4">
+                                        <img src="images/Engame.webp" alt="Engame Akadémia" class="w-4/12 h-auto">
+                                        <img src="images/Day1.webp" alt="Day1 Capital" class="w-4/12 h-auto">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
 
 </body>
+
+<Footer />
