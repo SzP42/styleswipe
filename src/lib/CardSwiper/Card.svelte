e771943b1219e5a18b1e0231e7a639916fc12d2b
AlmaDav <alma.fuzi.david@gmail.com>
2024-09-12 15:07:49 +0200

    Fuck, go back

--- src/lib/CardSwiper/Card.svelte
+++ src/lib/CardSwiper/Card.svelte
@@ -15,18 +15,17 @@
 	class="w-full h-full absolute cursor-grab ease-in-out rounded-xl touch-none select-none bg-[#ffffff] border-2 border-dark_bg {color} overflow-hidden flex justify-center mt-6 mb-6"
 	bind:this={element}
 >
-	<div class="grid grid-cols-1">
-		{#key image}
-		{#if image}
-		<img class="w-full h-full rounded-xl object-scale-down" src={image} alt={title} />
-		{/if}
-		{/key}
-		<div class="px-4 pb-8 bottom-0 flex justify-center bg-transparent">
-			<div class="w-8/12">
-				<a href={productLink} target="_blank">
-					<h3 class="text-xl md:text-2xl p-3 rounded-3xl bg-dark_bg  text-bright_text font-semibold text-wrap flex justify-center">{title}</h3> 
-				</a>
-			</div>
+{#key image}
+{#if image}
+	<img class="w-full h-full rounded-xl object-scale-down" src={image} alt={title} />
+{/if}
+{/key}
+	<div class="absolute inset-0 rounded-b-xl"></div>
+	<div class="p-4 pb-8 absolute bottom-0 max-w-8/12">
+		<div class="flex flex-col justify-start col-span-2">
+			<a href={productLink} target="_blank">
+				<h3 class="text-xl md:text-2xl p-3 rounded-3xl bg-dark_bg  text-bright_text font-semibold text-wrap flex justify-center">{title}</h3> 
+			</a>
 		</div>
 	</div>
 </div>
