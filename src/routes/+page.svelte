6077e4f154488b4d2e76c9f54438ec8be86cbe70
AlmaDav <alma.fuzi.david@gmail.com>
2024-09-18 18:20:07 +0200

    Added CTA

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,19 @@
 </script>
 
 <Navbar />
-<section class="py-16 bg-bright_bg justify-center flex text-center lg:py-32"> <!--Call to action-->
+<section class="bg-bright_bg"> <!--CTA-->
+    <div class="text-dark_bg font-semibold text-3xl mx-8 pt-8">
+        <h1 class="justify-start flex">Find your</h1>
+        <h1 class="justify-end   flex">dream wardrobe!</h1>
+    </div>
+    <div>
+        <a href="/app"><img src="videos/swipe.gif" alt=""></a>
+        <a href="/app" class="flex justify-center">
+            <button class="border-2 shadow-sm hover:shadow-black hover:shadow-md shadow-dark_bg border-black rounded-3xl hover:bg-white hover:text-dark_text bg-dark_bg text-bright_text text-2xl md:text-base md:w-40 p-4">Try it Yourself!</button>
+        </a>
+    </div>
+</section>
+<section class="py-16 bg-bright_bg justify-center flex text-center lg:py-32"> <!--Capsule Wardrobe-->
     <div class="container bg-white py-2">
         <div class="row gap-y-12">
             <div class="text-dark_text">
